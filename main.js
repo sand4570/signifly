b968ff871996644c02aa3257b3b39cd333a3204f
Chri704r <christinepiilmann8@gmail.com>
2021-11-06 23:59:50 +0100

    frontpage added

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-import './style.css'
+import "./sass/style.scss";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+//-----------change on scroll----------------
+window.onscroll = function () {
+	navScroll();
+};
+
+function navScroll() {
+	if (document.documentElement.scrollTop > 0) {
+		document.querySelector("nav").classList.add("scrolled");
+		document.querySelector("h1").classList.add("animate");
+		document.querySelector("#heading p").classList.add("animate");
+		document.querySelector("#h1_second").classList.add("animate");
+	} else {
+		document.querySelector("nav").classList.remove("scrolled");
+		document.querySelector("h1").classList.remove("animate");
+		document.querySelector("#heading p").classList.remove("animate");
+		document.querySelector("#h1_second").classList.remove("animate");
+	}
+
+	if (document.documentElement.scrollTop > 200) {
+		document.querySelector("#button_wrapper").classList.add("button_animate");
+	} else {
+		document.querySelector("#button_wrapper").classList.remove("button_animate");
+	}
+}
